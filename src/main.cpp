68394cc623dd2f830166bf102c480ced18ad11d3
Oleg Voloshin <oleg.voloshin@protonmail.com>
2019-05-13 23:32:23 +0300

    Change standart validation layers to older ones
    This new version of moltenVk causes an annoying
    segmentaion error when removing swapchain object.
    
    For now I'll be using 1.1.92.1

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 #include "VulkanUtilities.hpp"
 
 const std::vector<const char *> validationLayers = {
-    "VK_LAYER_KHRONOS_validation"};
+    "VK_LAYER_LUNARG_standard_validation"};
 
 const std::vector<const char *> deviceExtensions = {
     VK_KHR_SWAPCHAIN_EXTENSION_NAME};
